113771f6b43ac17db856ff4f4a788c58c624ec12
brallan <apcomptec@gmail.com>
2013-06-27 21:20:33 -0600

    Pruebas de validación y se muestra toda la info de prematricula correctamente

--- PHP/prematricula.php
+++ PHP/prematricula.php
@@ -37,36 +37,31 @@
 				session_start();
 				$connection = mysql_connect("localhost", "root","");
 				mysql_select_db("mydb", $connection);
-				// 				$query = "SELECT p.Cedula, c.NombreCurso, cu.CodigoCurso, cu.NombreCurso FROM Persona p INNER JOIN PreMatriculaEstudiante pe ON pe.PersonaId=p.PersonaId INNER JOIN Prematricula pr ON pr.PrematriculaId=pe.PrematriculaId INNER JOIN Curso cu ON cu.CursoId=pr.CursoId INNER JOIN  Requisitos r ON r.CursoId=cu.CursoId INNER JOIN Curso c ON c.CursoId=r.CursoRequisitoId INNER JOIN Matricula m ON m.PersonaId=p.PersonaId INNER JOIN Oferta o ON o.OfertaId=m.OfertaId INNER JOIN Estados e ON e.EstadoId=m.EstadoId WHERE e.Nombre='Aprobado' AND p.Cedula='".$_SESSION["cedula"]."' GROUP BY cu.NombreCurso";
-				// 				if ($row = mysql_fetch_array($result)){
-				// 					echo "<table border = '1'> \n";
-				// 					echo "<tr><td>Código Curso</td><td>Nombre Curso</td></tr> \n";
-				// 					do {
-				// 						echo "<tr><td>".$row["CodigoCurso"]."</td><td>".utf8_encode($row["NombreCurso"])."</td></tr> \n";
-				// 					} while ($row = mysql_fetch_array($result));
-				// 					echo "</table> \n";
-				// 				} else {
-				//
-				// 				}
-// 				$query = "SELECT Persona.Cedula,Curso.CodigoCurso,Curso.NombreCurso,Curso.Creditos,Horario.HorarioCurso,Horario.PersonasMatriculadas FROM Persona INNER JOIN PreMatriculaEstudiante,Prematricula,Curso,Horario WHERE Persona.PersonaId = PreMatriculaEstudiante.PersonaId AND PreMatriculaEstudiante.PrematriculaID = Prematricula.PrematriculaId AND Prematricula.CursoId = Curso.CursoId AND Curso.CursoId = Horario.CursoId AND Persona.Cedula = '".$_SESSION["cedula"]."'";
 				$query = "SELECT Persona.Cedula,Curso.CodigoCurso,Curso.NombreCurso,Curso.Creditos,Hora.Hora,Horario.PersonasMatriculadas FROM Persona INNER JOIN PreMatriculaEstudiante,Prematricula,Curso,Horario,Hora WHERE Persona.PersonaId = PreMatriculaEstudiante.PersonaId AND PreMatriculaEstudiante.PrematriculaID = Prematricula.PrematriculaId AND Prematricula.CursoId = Curso.CursoId AND Curso.CursoId = Horario.CursoId AND Horario.HorarioId = Hora.HorarioId AND Persona.Cedula = '".$_SESSION["cedula"]."'";
 				$result = mysql_query($query);
 
 				if ($row = mysql_fetch_array($result)){
+				echo "<form action='upload.php' method='POST'>";
 				echo "<table border = '1'> \n";
 				echo "<tr><td><b>Código Curso</b></td><td><b>Nombre Curso</b></td><td><b>Horario</b></td><td><b>Personas matriculadas</b></td></tr>";
+				$i = 1;
 				while($array= mysql_fetch_array($result))
 
 				{
 					echo "<tr>
 						<td>".$array['CodigoCurso']."</td>".
-						"<td align='left'>"."<input type='checkbox' name='card[]' value='".$array['NombreCurso']."' nonchecked>".utf8_encode($array['NombreCurso'])."</td>".
+						"<td align='left'>"."<input type='checkbox' name='card[]' value='".$i."' nonchecked>".utf8_encode($array['NombreCurso'])."</td>".
 						"<td>".$array['Hora'].
 						"<td>".$array['PersonasMatriculadas']. "</td>".
 						"</tr>";
+					echo $i;
 				}
 
-				echo "</table> \n";
+				echo "</table> \n";				
+				echo "<p>";
+				echo "<input type='submit' value='Ingresar datos' name='btnUploadData'/>";
+				echo "</p>";
+				echo "</form>";
 			}else{
 				echo "¡ No se ha encontrado ningún registro !";
 			} ?>
---
+++ PHP/upload.php
@@ -0,0 +1,11 @@
+<?php 
+var_dump($_POST);
+// var_dump($_GET["array"]);
+if (isset($_POST["card"]))
+{
+	echo "fuuaaaa";
+}
+else{
+// 	echo 
+}
+?>
